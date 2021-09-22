600f0ef36e1f29a755175cb4a3c324c2414a6f8e
ihValery <ihValery@gmail.com>
2021-09-22 17:35:54 +0300

    Initial Commit

---
+++ InfiniteCarousel/InfiniteCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteCarouselApp.swift
+//  InfiniteCarousel
+//
+//  Created by Валерий Игнатьев on 22.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
