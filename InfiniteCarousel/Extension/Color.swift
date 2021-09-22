93848078ed667517a549b29522fb428bbe80e3f2
ihValery <ihValery@gmail.com>
2021-09-22 18:32:09 +0300

    Create infinite carusel

---
+++ InfiniteCarousel/Extension/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  InfiniteCarousel
+//
+//  Created by Валерий Игнатьев on 22.09.2021.
+//
+
+import SwiftUI
+
+extension Color {
+    static var blueApp = Color("blueApp")
+    static var purpleApp = Color("purpleApp")
+}
---
+++ InfiniteCarousel/View/Home.swift
@@ -0,0 +1,21 @@
+//
+//  Home.swift
+//  InfiniteCarousel
+//
+//  Created by Валерий Игнатьев on 22.09.2021.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+            .previewDevice("iPhone 13 Pro")
+    }
+}
